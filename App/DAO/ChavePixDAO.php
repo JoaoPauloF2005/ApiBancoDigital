7df8ed4cb66d90aba0c59ea26c1c37ede97f73a5
joao <hypergamerfrr@gmail.com>
2023-05-01 12:53:52 -0300

    Finalizando ChavePix

--- App/DAO/ChavePixDAO.php
+++ App/DAO/ChavePixDAO.php
@@ -3,57 +3,79 @@
 namespace App\DAO;
 
 use App\Model\ChavePixModel;
+use \PDO;
 
 class ChavePixDAO extends DAO
 {
     public function __construct()
     {
-        parent::__construct();       
+        parent::__construct();
+    }
+
+    public function insert(ChavePixModel $model)
+    {
+        $sql = "INSERT INTO chave_pix (chave, tipo, id_conta) VALUE (?, ?, ?)";
+
+        $stmt = $this->conexao->prepare($sql);
+
+        $stmt->bindValue(1, $model->chave);
+        $stmt->bindValue(2, $model->tipo);
+        $stmt->bindValue(3, $model->id_conta);
+
+        $stmt->execute();
+    }
+
+    public function update(ChavePixModel $model)
+    {
+        $sql = "UPDATE chave_pix SET chave = ?, tipo = ?, id_conta = ? WHERE id = ?";
+
+        $stmt = $this->conexao->prepare($sql);
+
+        $stmt->bindValue(1, $model->chave);
+        $stmt->bindValue(2, $model->tipo);
+        $stmt->bindValue(3, $model->id_conta);
+        $stmt->bindValue(4, $model->id);
+
+        $stmt->execute();
     }
 
     public function select()
     {
-        $sql = "SELECT * FROM chave_pix ";
+        $sql = "SELECT cp.*, co.nome as conta_nome
+                FROM chave_pix cp
+                JOIN conta c ON c.id = cp.id_conta
+                JOIN correntista co ON co.id = c.id_correntista";
 
         $stmt = $this->conexao->prepare($sql);
+
         $stmt->execute();
 
-        return $stmt->fetchAll(DAO::FETCH_CLASS);
+        return $stmt->fetchAll(PDO::FETCH_CLASS);
     }
 
-    public function insert(ChavePixModel $m) : bool
+    public function selectById(int $id)
     {
-        $sql = "INSERT INTO chave_pix (chave, tipo, , senha) VALUES (?, ?, ?, ?) ";
-
-        $stmt = $this->conexao->prepare($sql);
-        $stmt->bindValue(1, $m->nome);
-        $stmt->bindValue(2, $m->cpf);
-        $stmt->bindValue(3, $m->data_nasc);
-        $stmt->bindValue(4, $m->senha);
-
-        return $stmt->execute();
-    }
-
-    public function update(CorrentistaModel $m)
-    {
-        $sql = "UPDATE correntista SET nome=?, cpf=?, data_nasc=?, senha=? WHERE id=? ";
-
-        $stmt = $this->conexao->prepare($sql);
-        $stmt->bindValue(1, $m->nome);
-        $stmt->bindValue(2, $m->cpf);
-        $stmt->bindValue(3, $m->data_nasc);
-        $stmt->bindValue(4, $m->senha);
-        $stmt->bindValue(3, $m->id);
-
-        return $stmt->execute();
-    }
-
-    public function delete(int $id) : bool
-    {
-        $sql = "DELETE FROM correntista WHERE id = ? ";
+        $sql = "SELECT cp.*, co.nome as conta_nome
+                FROM chave_pix cp
+                JOIN conta c ON c.id = cp.id_conta
+                JOIN correntista co ON co.id = c.id_correntista
+                WHERE cp.id = ?";
 
         $stmt = $this->conexao->prepare($sql);
         $stmt->bindValue(1, $id);
-        return $stmt->execute();
+
+        $stmt->execute();
+
+        return $stmt->fetchObject("App\Model\ChavePixModel");
+    }
+
+    public function delete(int $id)
+    {
+        $sql = "DELETE FROM chave_pix WHERE id = ?";
+
+        $stmt = $this->conexao->prepare($sql);
+        $stmt->bindValue(1, $id);
+
+        $stmt->execute();
     }
 }
